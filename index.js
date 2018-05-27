efe05975682ece856ff2736bd010311050243add
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 11:23:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Create a function theBeatlesPlay, which accepts an array of musicians and an array of instruments
 // The function body should contain an empty array stored in a variable
 // The function should also contain a for loop which loops over the array of musicians
+// The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: "John Lennon plays guitar"
