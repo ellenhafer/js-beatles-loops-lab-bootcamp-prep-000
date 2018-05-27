77877fa553f0d207894020004b2e7077d43a53e3
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 11:23:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,4 @@
 // The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: "John Lennon plays guitar"
 // This string should be added to the empty array you created.
 // The loop should make the same sentence for every member of the musicians array.
+// The function should return the array of new strings.
