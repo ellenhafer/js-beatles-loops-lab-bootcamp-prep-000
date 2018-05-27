128f04887ea5cf8e3499aa8c3178eba20070184e
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 11:22:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,3 @@
+// Create a function theBeatlesPlay, which accepts an array of musicians and an array of instruments
+// The function body should contain an empty array stored in a variable
+// The function should also contain a for loop which loops over the array of musicians
