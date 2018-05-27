48abd9cbbf0ed49ced2b5814973c3e9c78ab992c
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 11:43:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,3 @@ function theBeatlesPlay(musicians, instruments){
   // Return the new array
   return arr;
 }
-// The function body should contain an empty array stored in a variable
-// The function should also contain a for loop which loops over the array of musicians
-// The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: "John Lennon plays guitar"
-// This string should be added to the empty array you created.
-// The loop should make the same sentence for every member of the musicians array.
-// The function should return the array of new strings.
