d40b718444b98e667c5477b77c2d57087d9170f3
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 11:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// Create a function theBeatlesPlay, which accepts an array of musicians and an array of instruments
+// Create a function theBeatlesPlay, which accepts two array parameters
+function theBeatlesPlay(musicians, instruments){
+  // Create an empty array stored in a variable
+  var arr = [];
+  // Create a for loop which loops over the musicians array
+  for(let i = 0; i < musicians.length; i++) {
+    // Add string to empty array
+    arr.pop(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  // Return the new array
+  return arr;
+}
 // The function body should contain an empty array stored in a variable
 // The function should also contain a for loop which loops over the array of musicians
 // The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: "John Lennon plays guitar"
