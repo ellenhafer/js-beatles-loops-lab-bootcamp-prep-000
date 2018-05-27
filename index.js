3519e2dfd99f8bfec13979251d77ea5a764d008a
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 11:23:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 // The function body should contain an empty array stored in a variable
 // The function should also contain a for loop which loops over the array of musicians
 // The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: "John Lennon plays guitar"
+// This string should be added to the empty array you created.
